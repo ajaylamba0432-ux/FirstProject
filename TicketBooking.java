1e9c270ce7066e5dd3af6377f5960f6df2b1b3f0
Ajay Kumar <ajaylamba0432@gmail.com>
2026-05-19 17:39:07 +0530

    First Commit

---
+++ TicketBooking.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TicketBooking {
+    public static void main(String[] args) {
+        boolean ch = true;
+        Scanner sc = new Scanner(System.in);
+        while (ch) {
+            try {
+                System.out.println("Press 1 for buying a ticket \nPress 2 for cancel a ticket \nPress 3 for exit");
+                System.out.println("Enter Your choice");
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter number of tickets");
+                        int ticket = sc.nextInt();
+                        if (ticket > 6) {
+                            throw new Exception("Please buy ticket less than 6");
+                        } else {
+                            System.out.println("You succesfully buy " + ticket + " tickets");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Ticket is cancelled succesfully");
+                        break;
+                    case 3:
+                        System.out.println("You exit succesfully");
+                        ch = false;
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        sc.close();
+    }
+}
